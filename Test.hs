40f20a56aae36e211617651fab4f6e1b55059cfd
Chaitra-M-Sharada <61283383+Chaitra-M-Sharada@users.noreply.github.com>
2020-02-23 20:49:00 +0530

    First Commit Message

---
+++ Test.hs
@@ -0,0 +1,6 @@
+module Main where
+main :: IO ()
+main = do
+       putStrLn "Hello, World!"
+       putStrLn "***** This is my first HASKELL PROGRAM *****"
+       putStrLn "+++++ Welcome to FUNCTIONAL PROGRAMMING +++++"
